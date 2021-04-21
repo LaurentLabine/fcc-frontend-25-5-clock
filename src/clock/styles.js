794ef2994ff85a2f91420f1a5d051f38f7e0ed07
Laurent Labine <laurentlabine@gmail.com>
2021-04-21 17:54:58 +0200

    Created new component files
    Also added global styles from Styled COmponents

---
+++ src/clock/styles.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    #root {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+    font-family: "Roboto";
+    color: white;
+    }
+
+    html {
+        margin: 0;
+        padding:0;
+    }
+
+    body {
+        // background-image: url("https://user-images.githubusercontent.com/20836468/115224640-67059d00-a10d-11eb-8a10-9d799e4fc93d.jpg");
+        background-color: black !important;
+        background-size: 100%;
+        background-position: center;
+    }
+
+    #controls {
+        color: white;
+        position: relative;
+        bottom: 240px;
+        display: float;
+        float: right;
+    }
+    `;
