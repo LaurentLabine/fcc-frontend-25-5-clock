ba8770c4c4ebdd26e6d747e23dce8bb37db68621
Laurent Labine <laurentlabine@gmail.com>
2021-04-22 12:15:02 +0200

    Played around with fonts and added Codedby

--- src/clock/clock.jsx
+++ src/clock/clock.jsx
@@ -16,24 +16,51 @@ const Control = props => {
   return (
     <ControlContainer id={breakLabelId}>
       <ControlsHeader>{id}</ControlsHeader>
-      <ValueDisplay id={controlId}>{value}</ValueDisplay>
-      <Button
+      <ControlsValueDisplay id={controlId}>{value}</ControlsValueDisplay>
+      <ControlsButtonsStyle
         type="button"
+        className="glow-on-hover"
         id={decrementId}
         disabled={disabled}
         onClick={decrement}
       >
         -
-      </Button>
-      <Button
+      </ControlsButtonsStyle>
+      <ControlsButtonsStyle
         type="button"
+        className="glow-on-hover"
         id={incrementId}
         disabled={disabled}
         onClick={increment}
       >
         +
-      </Button>
+      </ControlsButtonsStyle>
     </ControlContainer>
+  );
+};
+
+const TimerHeader = props => {
+  const { timerStatus, mins, secs } = props;
+  return (
+    <TimerHeaderStyle id="counterStatus">
+      <TimerStatus id="timer-label">{timerStatus}</TimerStatus>
+      <TimerValueDisplay id="time-left" className="Counter">
+        {mins}:{secs}
+      </TimerValueDisplay>
+    </TimerHeaderStyle>
+  );
+};
+const TimerControls = props => {
+  const { isActive, toggle, reset } = props;
+  return (
+    <TimerControlsStyle id="buttons-Section">
+      <TimerButtonsStyle className="glow-on-hover" type="button" id="start_stop" onClick={toggle}>
+        {isActive ? 'Stop' : 'Start'}
+      </TimerButtonsStyle>
+      <TimerButtonsStyle className="glow-on-hover" type="button" id="reset" onClick={reset}>
+        Reset
+      </TimerButtonsStyle>
+    </TimerControlsStyle>
   );
 };
 
@@ -94,30 +121,30 @@ class Clock extends Component {
   toggle = () => {
     const { isActive, intervalId } = this.state;
 
-    if(isActive) {
-      clearInterval(intervalId)
+    if (isActive) {
+      clearInterval(intervalId);
     } else {
       this.setState({
         intervalId: setInterval(() => {
           const { time, isOnBreak, sessionLength, breakLength } = this.state;
-          if(time !== 0){
+          if (time !== 0) {
             this.setState({
-              time : time-1,
-            })
-          }else{
+              time: time - 1
+            });
+          } else {
             this.audioBeep.play();
             this.setState({
-              time : isOnBreak? sessionLength*60 : breakLength*60,
-              isOnBreak : !isOnBreak
-            })
-        }
-      }, 1000)
+              time: isOnBreak ? sessionLength * 60 : breakLength * 60,
+              isOnBreak: !isOnBreak
+            });
+          }
+        }, 1000)
       });
     }
 
     this.setState({
-      isActive : !isActive
-    })
+      isActive: !isActive
+    });
   };
 
   reset = () => {
@@ -148,44 +175,44 @@ class Clock extends Component {
       secs: formatNumber(Math.floor(time % 60))
     };
 
-    let timerLabel = isActive ? 'Started': 'Stopped'
-    if(isActive && isOnBreak)
-      timerLabel = 'Break Time'
+    let Status = isActive ? 'Started' : 'Stopped';
+    if (isActive && isOnBreak) Status = 'Break';
 
     return (
       <>
         <GlobalStyle />
-        <CounterContainer className="Clock">
-          <CounterStatus>
-            <ControlsHeader id="timer-label">
-              {timerLabel}
-            </ControlsHeader>
-            <ValueDisplay id="time-left" className="Counter">
-              {counter.mins}:{counter.secs}
-            </ValueDisplay>
-          </CounterStatus>
-          <Control
-            id="break"
-            value={breakLength}
-            disabled={isActive}
-            increment={this.incBreakLength}
-            decrement={this.decBreakLength}
-          />
-          <Control
-            id="session"
-            value={sessionLength}
-            disabled={isActive}
-            increment={this.incSessionLength}
-            decrement={this.decSessionLength}
-          />
-          <TimerControls>
-            <Button type="button" id="start_stop" onClick={this.toggle}>
-              {isActive ? 'Stop' : 'Start'}
-            </Button>
-            <Button type="button" id="reset" onClick={this.reset}>
-              Reset
-            </Button>
-          </TimerControls>
+        <CounterContainer>
+          <Border>
+            <CounterContainer className="Clock">
+              <TimerHeader
+                timerStatus={Status}
+                mins={counter.mins}
+                secs={counter.secs}
+              />
+              <ControlsContainer>
+                <Control
+                  id="break"
+                  value={breakLength}
+                  disabled={isActive}
+                  increment={this.incBreakLength}
+                  decrement={this.decBreakLength}
+                />
+                <Control
+                  id="session"
+                  value={sessionLength}
+                  disabled={isActive}
+                  increment={this.incSessionLength}
+                  decrement={this.decSessionLength}
+                />
+              </ControlsContainer>
+              <TimerControls
+                isActive={isActive}
+                reset={this.reset}
+                toggle={this.toggle}
+              />
+            </CounterContainer>
+          </Border>
+          <CodedBy>Designed and implemented by Laurent Labine in 2021</CodedBy>
         </CounterContainer>
         <audio
           id="beep"
@@ -202,73 +229,75 @@ class Clock extends Component {
 
 export default Clock;
 
-const Button = styled.button`
+const ControlsButtonsStyle = styled.button`
   cursor: pointer;
   background: transparent;
-  font-size: 16px;
-  border-radius: 3px;
-  color: palevioletred;
-  border: 2px solid palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
+  font-size: 32px;
+  width: 50px;
+  margin: 0 10px;
+  color: #2CFF51;
   transition: 0.5s all ease-out;
+  font-family: "Montserrat";
+`;
 
-  &:hover {
-    background-color: palevioletred;
-    color: white;
-  }
+const TimerButtonsStyle = styled.button`
+display: inline-block;  
+cursor: pointer;
+  width: 160px;
+  background: transparent;
+  font-size: 32px;
+  color: #2CFF51;
+  margin: 0.2em 0.2em;
+  transition: 0.5s all ease-out;
+  text-align: center;
+  font-family: "Montserrat";
 `;
 
 const ControlContainer = styled.div`
-  display: inline-block;
-  cursor: pointer;
-  background: transparent;
+  width:144px;
   font-size: 16px;
-  border-radius: 3px;
-  color: palevioletred;
-  border: 2px solid palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
+  margin: 0 0.3em;
   transition: 0.5s all ease-out;
   text-align: center;
 `;
 
-const CounterContainer = styled.div`
-  display: inline-block;
-  cursor: pointer;
+const ControlsContainer = styled.div`
+  display: flex;
   background: transparent;
   font-size: 16px;
-  border-radius: 3px;
-  color: palevioletred;
-  border: 2px solid palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
   transition: 0.5s all ease-out;
 `;
 
-const CounterStatus = styled.div`
+const Border = styled.div`
   display: block;
-  cursor: pointer;
-  background: transparent;
+  background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
   font-size: 16px;
-  border-radius: 3px;
-  color: palevioletred;
-  border: 2px solid palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
+  transition: 0.5s all ease-out;
+  border-radius: 30px;
+  padding: 5px;
+`;
+
+const CounterContainer = styled.div`
+  background: black;
+  border-radius: 30px;
+`;
+
+const TimerHeaderStyle = styled.div`
+  display: block;
+  background: transparent;
+  padding: 1em ;
   transition: 0.5s all ease-out;
   justify-content: center;
   vertical-align: center;
+  font-family: "Digitalt";
 `;
 
-const TimerControls = styled.div`
+const TimerControlsStyle = styled.div`
   display: flex;
-  cursor: pointer;
   background: transparent;
   font-size: 16px;
-  border-radius: 3px;
-  color: palevioletred;
-  border: 2px solid palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
   transition: 0.5s all ease-out;
   justify-content: center;
   vertical-align: center;
@@ -277,27 +306,62 @@ const TimerControls = styled.div`
 const ControlsHeader = styled.header`
   display: flex;
   width: 100%;
-  cursor: pointer;
   background: transparent;
-  font-size: 16px;
-  border-radius: 3px;
-  color: palevioletred;
-  border: 2px solid palevioletred;
+  font-size: 40px;
+  background: -webkit-linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
   transition: 0.5s all ease-out;
   justify-content: center;
   vertical-align: center;
 `;
 
-const ValueDisplay = styled.span`
+const TimerStatus = styled.header`
+display: flex;
+font-size: 72px;
+background: -webkit-linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+justify-content: center;
+vertical-align: center;
+`;
+
+const ControlsValueDisplay = styled.span`
   display: flex;
   width: 100%;
-  cursor: pointer;
-  background: transparent;
-  font-size: 16px;
-  border-radius: 3px;
-  color: palevioletred;
-  border: 2px solid palevioletred;
+  background: -webkit-linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size:52px;
+  padding: 10px 0;
   transition: 0.5s all ease-out;
   justify-content: center;
   vertical-align: center;
+  font-family: 'Digital System';
+`;
+
+const TimerValueDisplay = styled.span`
+  display: flex;
+  width: 100%;
+  background: -webkit-linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 90px;
+  transition: 0.5s all ease-out;
+  justify-content: center;
+  vertical-align: center;
+  font-family: 'Digital System';
+  padding-top:10px;
+`;
+
+const CodedBy = styled.span`
+  display: flex;
+  font-size: 12px;
+  // width:100px;
+  transition: 0.5s all ease-out;
+  justify-content: center;
+  vertical-align: center;
+  background: -webkit-linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 `;
--- src/clock/styles.js
+++ src/clock/styles.js
@@ -1,14 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+
+@font-face {font-family: "Digital System"; src: url("//db.onlinewebfonts.com/t/0b410990500bfc45409d16ad0b433a00.eot"); src: url("//db.onlinewebfonts.com/t/0b410990500bfc45409d16ad0b433a00.eot?#iefix") format("embedded-opentype"), url("//db.onlinewebfonts.com/t/0b410990500bfc45409d16ad0b433a00.woff2") format("woff2"), url("//db.onlinewebfonts.com/t/0b410990500bfc45409d16ad0b433a00.woff") format("woff"), url("//db.onlinewebfonts.com/t/0b410990500bfc45409d16ad0b433a00.ttf") format("truetype"), url("//db.onlinewebfonts.com/t/0b410990500bfc45409d16ad0b433a00.svg#Digital System") format("svg"); }
+@font-face {font-family: "Digitalt"; src: url("//db.onlinewebfonts.com/t/8a5d150d70ddee86408dd6a2c693b5c9.eot"); src: url("//db.onlinewebfonts.com/t/8a5d150d70ddee86408dd6a2c693b5c9.eot?#iefix") format("embedded-opentype"), url("//db.onlinewebfonts.com/t/8a5d150d70ddee86408dd6a2c693b5c9.woff2") format("woff2"), url("//db.onlinewebfonts.com/t/8a5d150d70ddee86408dd6a2c693b5c9.woff") format("woff"), url("//db.onlinewebfonts.com/t/8a5d150d70ddee86408dd6a2c693b5c9.ttf") format("truetype"), url("//db.onlinewebfonts.com/t/8a5d150d70ddee86408dd6a2c693b5c9.svg#Digitalt") format("svg"); }
+
     #root {
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100vh;
     width: 100%;
-    font-family: "Roboto";
     color: white;
+    font-family: 'Montserrat', sans-serif;
     }
 
     html {
@@ -17,17 +21,66 @@ export default createGlobalStyle`
     }
 
     body {
-        // background-image: url("https://user-images.githubusercontent.com/20836468/115224640-67059d00-a10d-11eb-8a10-9d799e4fc93d.jpg");
-        background-color: black !important;
+        background-color: black;
         background-size: 100%;
         background-position: center;
     }
 
-    #controls {
-        color: white;
-        position: relative;
-        bottom: 240px;
-        display: float;
-        float: right;
-    }
+    .glow-on-hover {
+      // width: 140px;
+      height: 50px;
+      border: none;
+      outline: none;
+      cursor: pointer;
+      position: relative;
+      z-index: 0;
+      border-radius: 10px;
+  }
+  
+  .glow-on-hover:before {
+      content: '';
+      background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+      position: absolute;
+      top: -2px;
+      left:-2px;
+      background-size: 400%;
+      z-index: -1;
+      filter: blur(5px);
+      width: calc(100% + 4px);
+      height: calc(100% + 4px);
+      animation: glowing 20s linear infinite;
+      opacity: 0;
+      transition: opacity .3s ease-in-out;
+      border-radius: 10px;
+  }
+  
+  .glow-on-hover:active {
+      color: #000
+  }
+  
+  .glow-on-hover:active:after {
+      background: transparent;
+  }
+  
+  .glow-on-hover:hover:before {
+      opacity: 1;
+  }
+  
+  .glow-on-hover:after {
+      z-index: -1;
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background: black;
+      left: 0;
+      top: 0;
+      border-radius: 10px;
+  }
+  
+  @keyframes glowing {
+      0% { background-position: 0 0; }
+      50% { background-position: 400% 0; }
+      100% { background-position: 0 0; }
+  }
     `;
